8ed0a0ed30b8b782efa46d50bee4db6df2c963cc
michelwijnja@gmail.com <michelwijnja@gmail.com>
2022-11-08 09:25:50 +0100

    first commit

---
+++ devices/e1000/update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Need 3 arguments: 1) kernel source dir, 2) previous version, 3) version to add"
+    exit 1
+fi
+
+KERNELDIR=$1
+PREVER=$2
+KERNELVER=$3
+
+set -x
+
+for f in $KERNELDIR/drivers/net/ethernet/intel/e1000/*.[ch]; do
+    echo $f
+    b=$(basename $f)
+    o=${b/\./-$KERNELVER-orig.}
+    e=${b/\./-$KERNELVER-ethercat.}
+    cp -v $f $o
+    chmod 644 $o
+    cp -v $o $e
+    op=${b/\./-$PREVER-orig.}
+    ep=${b/\./-$PREVER-ethercat.}
+    diff -u $op $ep | patch -p1 $e
+    sed -i s/$PREVER-ethercat.h/$KERNELVER-ethercat.h/ $e
+    git add $o $e
+done
---
+++ devices/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+KERNELDIR=/data/kernel/linux-4.4.162
+PREVER=3.16
+KERNELVER=4.4
+
+for f in $KERNELDIR/drivers/net/ethernet/{realtek/8139too,realtek/r8169,intel/e100}.c; do
+    echo $f
+    b=$(basename $f)
+    o=${b/\./-$KERNELVER-orig.}
+    e=${b/\./-$KERNELVER-ethercat.}
+    cp -v $f $o
+    chmod 644 $o
+    cp -v $o $e
+    op=${b/\./-$PREVER-orig.}
+    ep=${b/\./-$PREVER-ethercat.}
+    diff -u $op $ep | patch -p1 $e
+    git add $o $e
+done
